bcaaf14465476c5aab097bd16e7bd1cf15fdb1b8
Kate Hedgpeth <katehedgpeth@gmail.com>
2023-02-21 11:02:49 -0500

    move api response parsing to separate helper method

--- lib/bird_song/services/ebird.ex
+++ lib/bird_song/services/ebird.ex
@@ -1,12 +1,10 @@
 defmodule BirdSong.Services.Ebird do
+  alias BirdSong.Services.Helpers
   alias __MODULE__.Observation
 
   @token :bird_song
          |> Application.compile_env(:ebird)
          |> Keyword.fetch!(:token)
-
-  @type response(t) ::
-          {:ok, t} | {:error, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
 
   def url(endpoint) do
     :bird_song
@@ -17,7 +15,7 @@ defmodule BirdSong.Services.Ebird do
     |> Path.join()
   end
 
-  @spec get_recent_observations(String.t()) :: response(List.t(Observation.t()))
+  @spec get_recent_observations(String.t()) :: Helpers.api_response(List.t(Observation.t()))
   def get_recent_observations("" <> region) do
     "data/obs"
     |> Path.join([region])
@@ -30,22 +28,10 @@ defmodule BirdSong.Services.Ebird do
     end
   end
 
-  @spec send_request(String.t(), List.t({String.t(), any})) :: response(List.t())
+  @spec send_request(String.t(), List.t({String.t(), any})) :: Helpers.api_response(List.t())
   defp send_request(url, params) do
     url
     |> HTTPoison.get([{"x-ebirdapitoken", @token}], params: params)
-    |> case do
-      {:ok, %HTTPoison.Response{status_code: 200, body: "" <> body}} ->
-        Jason.decode(body)
-
-      {:ok, %HTTPoison.Response{status_code: 404, request: %HTTPoison.Request{url: url}}} ->
-        {:error, {:not_found, url}}
-
-      {:ok, %HTTPoison.Response{} = response} ->
-        {:error, {:bad_response, response}}
-
-      error ->
-        error
-    end
+    |> Helpers.parse_api_response()
   end
 end
---
+++ lib/bird_song/services/helpers.ex
@@ -0,0 +1,21 @@
+defmodule BirdSong.Services.Helpers do
+  @type api_response(t) ::
+          {:ok, t}
+          | {:error, HTTPoison.Response.t()}
+          | {:error, HTTPoison.Error.t()}
+
+  @spec parse_api_response({:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}) ::
+          api_response(any)
+  def parse_api_response({:ok, %HTTPoison.Response{status_code: 200, body: "" <> body}}),
+    do: Jason.decode(body)
+
+  def parse_api_response(
+        {:ok, %HTTPoison.Response{status_code: 404, request: %HTTPoison.Request{url: url}}}
+      ),
+      do: {:error, {:not_found, url}}
+
+  def parse_api_response({:ok, %HTTPoison.Response{} = response}),
+    do: {:error, {:bad_response, response}}
+
+  def parse_api_response(error), do: error
+end
