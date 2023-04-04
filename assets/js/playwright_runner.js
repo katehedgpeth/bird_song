14abd161cb419746a8939449a5788f1ee3693f41
Kate Hedgpeth <katehedgpeth@gmail.com>
2023-04-04 10:03:57 -0400

    Sign in to ebird instead of using storage state

--- assets/js/playwright_runner.js
+++ assets/js/playwright_runner.js
@@ -1,31 +1,22 @@
-const File = require("fs");
-const {chromium} = require("playwright");
-const STORAGE_STATE = require("./playwright_auth.json");
-const [, , BASE_URL, STORAGE_STATE_PATH] = process.argv;
+const { chromium } = require("playwright");
+const [, , BASE_URL] = process.argv;
 const USERNAME = process.env.EBIRD_USERNAME;
 const PASSWORD = process.env.EBIRD_PASSWORD;
 
-if (!BASE_URL || !STORAGE_STATE_PATH) {
+if (!BASE_URL) {
   console.log({argv: process.argv, error: "Received less arguments than expected"})
   throw new Error()
 }
 
 const timeout = 3_000;
 
-// if (!USERNAME) throw new Error("missing env var: EBIRD_USERNAME")
-// if (!PASSWORD) throw new Error("missing env var: EBIRD_PASSWORD")
-
-
-
 const sendMessage = (data) => {
   console.log("message=" + data)
 }
 
-
-
 (async () => {
   const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext({ storageState: STORAGE_STATE });
+  const context = await browser.newContext();
   const page = await context.newPage();
 
   const shutdown = async () => {
@@ -35,8 +26,9 @@ const sendMessage = (data) => {
     process.exit(0);
   }
 
-  const sendErrorMessage = (message) => {
-    return sendMessage(JSON.stringify(message))
+  const sendErrorMessage = async (message) => {
+    await page.screenshot({path: "./screenshot.png"});
+    return sendMessage(JSON.stringify(message));
   }
 
   const continueIfGoodResponse = async (response, url, callback) =>
@@ -67,16 +59,16 @@ const sendMessage = (data) => {
   }
 
   const signIn = async () => {
+    await page.locator("a.Header-link").getByText("Sign in").click();
     await page.type("#input-user-name", USERNAME);
     await page.type("#input-password", PASSWORD);
-    return page.click("#form-submit");
+    await page.click("#form-submit");
+    await page.waitForLoadState("load")
+    return waitForResultsList();
   }
 
   const waitForResultsList = async () => {
     await page.waitForLoadState("load");
-    if (await page.isVisible("#input-user-name")) { 
-      await signIn();
-    }
     return locators.resultsList.waitFor({ timeout })
       .then(() => sendMessage("ready_for_requests"))
       .catch((error) =>
@@ -90,14 +82,14 @@ const sendMessage = (data) => {
   const connectToSite = async () => {
     const url = BASE_URL + "/catalog?view=list"
     const response = await page.goto(url);
-    return continueIfGoodResponse(response, url, waitForResultsList)
+    return continueIfGoodResponse(response, url, signIn);
   }
 
   const parseJSON = (json) => {
     try {
       return JSON.parse(json)
     } catch (error) {
-      sendErrorMessage({error: "json_parse_error", message: error.message, input: json})
+      return sendErrorMessage({error: "json_parse_error", message: error.message, input: json})
     }
   }
 
@@ -112,12 +104,15 @@ const sendMessage = (data) => {
 
     const url = BASE_URL + "/api/v2/search";
 
-    const response = await page.request.get(url, {
-      params: params({
-        initialCursorMark: initial_cursor_mark,
-        taxonCode: code
-      })
-    });
+    const response = await page.request.get(
+      url,
+      {
+        params: params({
+          initialCursorMark: initial_cursor_mark,
+          taxonCode: code
+        })
+      }
+    );
 
     const sendResponseMessage = async () => {
       const results = parseJSON((await response.body()).toString());
@@ -133,7 +128,7 @@ const sendMessage = (data) => {
   }
 
   const handleInput = async (buffer) => {
-    const message = buffer.toString()
+    const message = buffer.toString().trim()
     if (message === "connect") {
       return await connectToSite();
     }
