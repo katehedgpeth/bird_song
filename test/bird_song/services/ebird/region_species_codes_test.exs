ca0dbe79b029d9b57aad25eec36c8b995269d722
Kate Hedgpeth <katehedgpeth@gmail.com>
2023-05-05 14:46:52 -0400

    rewrite Ebird.RegionSpeciesCodes test, and rename test file

---
+++ test/bird_song/services/ebird/region_species_codes_test.exs
@@ -0,0 +1,29 @@
+defmodule BirdSong.Services.Ebird.RegionSpeciesCodesTest do
+  use BirdSong.SupervisedCase, async: true
+
+  alias BirdSong.{
+    MockEbirdServer,
+    Services.Ebird,
+    Services.Ebird.RegionSpeciesCodes
+  }
+
+  setup tags do
+    MockEbirdServer.setup(tags)
+
+    {:ok,
+     [
+       region: "US-NC-067",
+       worker: get_worker(Ebird, :RegionSpeciesCodes, tags)
+     ]}
+  end
+
+  describe "get_codes/2" do
+    test "returns %RegionSpeciesCodes.Response{} with a list of codes as strings", tags do
+      assert %{region: region, worker: worker} = Map.take(tags, [:region, :worker])
+      assert {:ok, response} = RegionSpeciesCodes.get_codes(region, worker)
+      assert %RegionSpeciesCodes.Response{} = response
+      assert response.region === "US-NC-067"
+      assert ["" <> _ | _] = response.codes
+    end
+  end
+end
